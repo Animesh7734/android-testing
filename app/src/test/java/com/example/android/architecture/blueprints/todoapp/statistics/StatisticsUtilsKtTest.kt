e4608b08b0383ace707c646d5a21fc95ec2b0bc2
ekincare <animesh.s@ekincare.com>
2022-06-21 15:36:15 +0530

    Adding Android Testing

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsKtTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsKtTest.kt
@@ -1,5 +1,35 @@
 package com.example.android.architecture.blueprints.todoapp.statistics
 
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import org.hamcrest.CoreMatchers.`is`
 import org.junit.Assert.*
+import org.junit.Test
 
-class StatisticsUtilsKtTest
+class StatisticsUtilsKtTest{
+    @Test
+    fun getActiveAndCompletedStats_noCompleted_returnsHundredZero(){
+        val task = listOf<Task>(Task("title","desec",isCompleted = false))
+
+        val result = getActiveAndCompletedStats(task)
+        assertThat(result.activeTasksPercent, `is`(100f))
+        assertThat(result.completedTasksPercent, `is`(0f))
+    }
+
+    @Test
+    fun getActiveAndCompleteStats_ifTaskIsEmpty_retrunzeros(){
+
+        val result = getActiveAndCompletedStats(emptyList())
+
+         assertThat(result.activeTasksPercent, `is`(0f))
+        assertThat(result.completedTasksPercent,`is`(0f))
+    }
+
+    @Test
+    fun getActiveAndCompleteStats_ifTaskIsNull_retrunzeros(){
+
+        val result = getActiveAndCompletedStats(null)
+
+        assertThat(result.activeTasksPercent, `is`(0f))
+        assertThat(result.completedTasksPercent,`is`(0f))
+    }
+}
--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,5 +1,60 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.Event
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.source.FakeTestRepository
+import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
+import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.nullValue
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.AdditionalMatchers.not
 
-class TasksViewModelTest
+
+/**
+ * @RunWith(AndroidJUnit4::class)
+ * A test runner is a JUnit component that runs tests. Without a test runner, your tests would not run.
+ * There's a default test runner provided by JUnit that you get automatically. @RunWith swaps out that default test runner.
+ * */
+
+class TasksViewModelTest{
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var tasksViewModel: TasksViewModel
+    private lateinit var tasksRepository: FakeTestRepository
+
+    @Before
+    fun setupViewModel(){
+        tasksRepository = FakeTestRepository()
+        val task1 = Task("Title1", "Description1")
+        val task2 = Task("Title2", "Description2", true)
+        val task3 = Task("Title3", "Description3", true)
+        tasksRepository.addTasks(task1, task2, task3)
+
+        tasksViewModel = TasksViewModel(tasksRepository)
+    }
+    @Test
+    fun addNewTask_setsNewTaskEvent(){
+
+        tasksViewModel.addNewTask() // Call for Test Event
+        val value = tasksViewModel.newTaskEvent.getOrAwaitValue() //Get Live Data
+        assertThat(value.getContentIfNotHandled(), (not(nullValue()))) //Checking code working fine or not
+
+    }
+    @Test
+    fun setFilterAllTasks_tasksAddViewVisible(){
+        tasksViewModel.setFiltering(TasksFilterType.ALL_TASKS)
+
+        assertThat(tasksViewModel.tasksAddViewVisible.getOrAwaitValue(), `is`(true))
+
+    }
+}
